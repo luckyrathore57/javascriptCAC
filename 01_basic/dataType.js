7c042010e4ed015a86bc2a660a774648f335122c
luckyrathore57 <es1210189@iitd.ac.in>
2023-10-11 18:24:23 +0530

    01 done

---
+++ 01_basic/dataType.js
@@ -0,0 +1,15 @@
+"use strict"//for work with latest format
+//++++++++++++++++++++++++primitive data type+++++++++++++++++++++
+let name="Lucky Rathore";
+let age = 21;
+let logged=true;
+let m_no=65432456789987654567890987654847478456789876n;
+let state;
+let country=null;
+
+console.log(typeof name);// string
+console.log(typeof age);//maxInt 2^53 // it can store decimal also
+console.log(typeof logged);//boolean
+console.log(typeof m_no);//big int
+console.log(typeof state);//undefined
+console.log(typeof country);//null->object

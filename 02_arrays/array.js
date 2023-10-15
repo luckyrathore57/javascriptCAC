483c1488e8f67251b8fce23dc8beddc1df3167d7
luckyrathore57 <es1210189@iitd.ac.in>
2023-10-16 02:38:59 +0530

    arrays working

---
+++ 02_arrays/array.js
@@ -0,0 +1,66 @@
+// array initialize
+
+const arr=[2,3,4,5,4];
+const arr2=[3,4,"lucky",35.4,'c'];
+const arr3=new Array(3,5,3,5,2,6);
+console.log(arr);
+// console.log(arr2);
+// console.log(arr3);
+
+
+console.log("---------");
+
+//function of arr
+
+
+// arr.push(4);
+// console.log(arr);
+// arr.push(3,5,33,6,25);
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+
+
+
+// while(arr.length!=0){
+//     arr.pop();
+// }
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+
+
+// arr.shift(); //remove first element
+// console.log(arr);
+// arr.unshift(8); //add element if first index
+// console.log(arr);
+
+// arr.splice(2,0,75); //add element at 2 index
+// console.log(arr);
+// arr.splice(2,1);
+// console.log(arr);    //delete element at index 2
+arr.splice(1,2);
+console.log(arr);
+
+
+// console.log(arr.includes(5));
+// console.log(arr.indexOf(5));
+
+// const narr=arr.join(); // convert arr into string
+// console.log(narr);
+// console.log(arr);
+
+// console.log("lucky" ,arr);
+
+
+
+
+
+
+
+
+
+
+
+
+
